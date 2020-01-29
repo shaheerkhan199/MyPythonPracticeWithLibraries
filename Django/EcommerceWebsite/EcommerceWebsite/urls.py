0e30b52408b736f10b1a306c43f074d4d1babdb0
Muhammad Shaheer Khan <m.shaheerkhan199@gmail.com>
2020-01-28 22:43:12 -0800

    Successfully fetched products from database

--- Django/EcommerceWebsite/EcommerceWebsite/urls.py
+++ Django/EcommerceWebsite/EcommerceWebsite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,5 +24,5 @@ urlpatterns = [
     path('shop/', include('Shop.urls')),
     path('ChatBot/', include('ChatBot.urls')),
     path('test/', include('TestApp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

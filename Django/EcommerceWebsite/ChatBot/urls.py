eb95c1c8a66f1b36f487f4de3574548ab8786747
Muhammad Shaheer Khan <m.shaheerkhan199@gmail.com>
2020-02-16 02:06:14 -0800

    After integrating chatbot but its not working

--- Django/EcommerceWebsite/ChatBot/urls.py
+++ Django/EcommerceWebsite/ChatBot/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='ChatBotIndex'),
     path('ChatWindow', views.render_chat_window, name='ChatWindow'),
+    path('giveResponse', views.get_bot_response, name='ChatBot_Response')
 ]
--- Django/EcommerceWebsite/ChatBot/views.py
+++ Django/EcommerceWebsite/ChatBot/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 # Create your views here.
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot import ChatBot
 
+shopping_bot = ChatBot(
+        "My Shopping Bot",
+        storage_adapter="chatterbot.storage.SQLStorageAdapter"
+    )
+
+#Now we are not using list trainer any more
+trainer = ChatterBotCorpusTrainer(shopping_bot)
+trainer.train("chatterbot.corpus.english")
 
 def index(request):
     # return HttpResponse("This is ChatBot Chat App")
@@ -11,3 +22,11 @@ def index(request):
 def render_chat_window(request):
     return render(request, 'chatWindow.html')
 
+
+def get_bot_response(request):
+    user_input = request.GET.get('msg')
+    print(user_input)
+    bot_response = shopping_bot.get_response(user_input)
+    print(bot_response)
+    output = {'botResponse': bot_response}
+    return render(request, 'chatWindow.html', output)

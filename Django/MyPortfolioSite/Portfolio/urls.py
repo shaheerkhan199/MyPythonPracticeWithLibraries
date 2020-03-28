8c1720af1a73a0370bacfcea6e7090be383936fb
Muhammad Shaheer Khan <m.shaheerkhan199@gmail.com>
2020-03-27 18:30:16 -0800

    After contact page in portfolio site

--- Django/MyPortfolioSite/Portfolio/urls.py
+++ Django/MyPortfolioSite/Portfolio/urls.py
@@ -2,4 +2,9 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('/', views.index, name='indexPage'),
+    path('/contact/', views.contact, name='contactPage'),
+    path('/about/', views.about, name='aboutPage'),
+    path('/resume/', views.resume, name='resumePage'),
+    path('/services/', views.services, name='servicesPage'),
+
 ]
--- Django/MyPortfolioSite/Portfolio/views.py
+++ Django/MyPortfolioSite/Portfolio/views.py
@@ -4,5 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    # return HttpResponse("This is my personal portfolio site")
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def resume(request):
+    return render(request, 'resume.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
